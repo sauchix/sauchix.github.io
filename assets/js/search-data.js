98d9f2d60901a33783e7f85f9441c0625576be43
sauchix <159938319+sauchix@users.noreply.github.com>
2026-03-28 00:05:34 +0000

    Deploying to gh-pages from @ sauchix/sauchix.github.io@9137905ec760c4cd6efe68e30705f1fa2cd22dd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-sau-chi-tang",
-    title: "Sau Chi Tang",
+    id: "nav-sau-chi",
+    title: "Sau Chi",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
